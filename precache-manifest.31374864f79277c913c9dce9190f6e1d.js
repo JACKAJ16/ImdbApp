1290c6925781ac3f8f29a806882c7adb9041bbcd
JACK <zhenya271@gmail.com>
2020-05-12 01:50:41 +0300

    adding build

---
+++ precache-manifest.31374864f79277c913c9dce9190f6e1d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a7c65aa52911d9f85d9a9542b494751c",
+    "url": "/ImdbApp/index.html"
+  },
+  {
+    "revision": "a3f2360ca77b58514b32",
+    "url": "/ImdbApp/static/css/main.e8aba8f6.chunk.css"
+  },
+  {
+    "revision": "9fb2e08fd68e55da35b6",
+    "url": "/ImdbApp/static/js/2.eb8c21e0.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ImdbApp/static/js/2.eb8c21e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3f2360ca77b58514b32",
+    "url": "/ImdbApp/static/js/main.72e5a896.chunk.js"
+  },
+  {
+    "revision": "0fe8b7c18bbef7d21100",
+    "url": "/ImdbApp/static/js/runtime-main.a662af9f.js"
+  }
+]);
